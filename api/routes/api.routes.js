fb2c2f9122f075a331d8675d2ad0f5e68f4c458c
barry <berlogan35@gmail.com>
2021-07-28 16:53:12 +0100

    Separated front-end from backend

--- api/routes/api.routes.js
+++
@@ -1,7 +0,0 @@
-const ProjectCtrl = require("../controllers/project.controller")
-const express = require("express")
-const router = express.Router();
-
-router.get("/projects", ProjectCtrl.getAllProjects)
-
-module.exports = router
