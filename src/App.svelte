388d8ff65991d7eb495e674998d14ed27ec5c424
barry <berlogan35@gmail.com>
2021-07-28 17:51:36 +0100

    Better title for Index.html

--- src/App.svelte
+++ src/App.svelte
@@ -13,10 +13,15 @@
 	}
 
 </script>
+<div class="intro">
+	<h1><a target="_blank" href="https://berrykarake.ml/about/about.html">Bertrand (Berry) Karake</a></h1>
+	<p class="text-dark">Hello There! Here are some of my favourite projects!</p>
+	<p class="text-dark">Feel free to go to <a target="_blank" href="https://github.com/yeerkus"> my Github</a> if you want the code</p>
+</div>
 
 <main>
 	{#await getProjects()}
-		<h4 class="text-dark">Fetching Berry's Projects...</h4>
+		<h4 class="text-success">Fetching Berry's Projects...</h4>
 		{:then project}
 			{#each project as prop}
 				<Card 
@@ -36,6 +41,9 @@
 		display: grid;
 		grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 		grid-gap: 1rem;
-		margin: 1rem;
+		margin: 3%;
+	}
+	.intro{
+		margin: 3%;
 	}
 </style>
